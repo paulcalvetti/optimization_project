a3b3756930ba2c74ed798f8a44d793edb68cfb24
Paul Calvetti <calvetti@mit.edu>
2020-04-26 13:17:47 -0400

    Added SB odds and expected Wins for 2019

--- process_games.py
+++ process_games.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import xlrd
 
+def get_data():
+    df_games = pd.read_excel('nfl_games_all.xlsx', sheet_name='2019 Games')
+    df_odds = pd.read_excel('nfl_games_all.xlsx', sheet_name='2019 Odds')
 
-df = pd.read_excel('nfl_games_all.xlsx', sheet_name='2019 Games')
-print(df)
+    #get list of team names
+    teams = df_odds['Team'].to_list()
+    return df_games, df_odds, teams
+
+if __name__ == '__main__':
+    df_games, df_odds, teams = get_data()
+    print(df_games)
+    print(df_odds)
+    print(teams)
+    print(len(teams))
