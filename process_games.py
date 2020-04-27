d00f434a383713195483d6cb9a40d9974c619bed
Paul Calvetti <calvetti@mit.edu>
2020-04-27 14:05:51 -0400

    Add ELO to calculate win probabilties

--- process_games.py
+++ process_games.py
@@ -4,14 +4,12 @@ import copy
 
 def get_data():
     df_games = pd.read_excel('nfl_games_all.xlsx', sheet_name='2019 Games')
-    df_odds = pd.read_excel('nfl_games_all.xlsx', sheet_name='2019 Odds')
+    df_elo = pd.read_excel('elo_data.xlsx')
 
-    #get list of team names
-    teams = df_odds['Team'].to_list()
     df_weekly_list = []
     for i in range(1, 18):
         df_weekly_list.append(df_games.loc[df_games['Week'] == i])
-    return df_games, df_weekly_list, df_odds, teams
+    return df_games, df_weekly_list, df_elo
 
 def expand_games(df):
     winners = df['Winner']
@@ -24,26 +22,27 @@ def expand_games(df):
     df2['Opponent'] = winners
     return pd.concat([df1,df2])
 
-def add_win_probs(df, df_odds):
+def add_win_probs(df, df_elo):
     win_prob_list = []
     for index, row in df.iterrows():
         team = row.loc['Team']
-        team_expected_win = df_odds.loc[df_odds['Team'] == team]['Expected Wins'].to_list()[0]
+        team_elo = df_elo.loc[df_elo['Team'] == team]['Elo'].to_list()[0]
         opponent = row['Opponent']
-        opp_expected_win = df_odds.loc[df_odds['Team'] == opponent]['Expected Wins'].to_list()[0]
-        win_prob = team_expected_win / (team_expected_win + opp_expected_win)
+        opp_elo = df_elo.loc[df_elo['Team'] == opponent]['Elo'].to_list()[0]
+        elo_diff = team_elo - opp_elo
+        win_prob = 1/((10**(-elo_diff/400))+1)
         win_prob_list.append(win_prob)
     df['Win Prob'] = win_prob_list
 
 
 
 def construct_dfs():
-    df_games, df_weekly_list, df_odds, teams = get_data()
+    df_games, df_weekly_list, df_elo = get_data()
     df_list = []
     for df in df_weekly_list:
         df_list.append(expand_games(df))
     for df in df_list:
-        add_win_probs(df, df_odds)
+        add_win_probs(df, df_elo)
     return df_list
 
 
